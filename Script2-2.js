e04b5825182d7e2636dce8c34c9a41053703a6aa
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2024-03-17 13:48:32 -0400

    Version 2.2

--- Script2-2.js
+++ Script2-2.js
@@ -11,6 +11,16 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
+document.addEventListener('wheel', function(event) {
+    let deltaY = event.deltaY;
+
+    if (deltaY > 0) {
+        moveNext(); // Si el desplazamiento es hacia abajo, mover hacia adelante
+    } else if (deltaY < 0) {
+        movePrev(); // Si el desplazamiento es hacia arriba, mover hacia atrás
+    }
+});
+
 document.addEventListener('click', function(event) {
     let windowWidth = window.innerWidth; // Ancho de la ventana del navegador
     let clickX = event.clientX; // Posición X del clic
@@ -26,10 +36,14 @@ document.addEventListener('click', function(event) {
 
 function moveNext() {
     let items = document.querySelectorAll('.item');
-    document.querySelector('.slide').appendChild(items[0]);
+    let slide = document.querySelector('.slide');
+    slide.appendChild(items[0]); // Mueve el primer elemento al final
+    slide.style.justifyContent = "flex-end"; // Alinea los elementos al extremo derecho
 }
 
 function movePrev() {
     let items = document.querySelectorAll('.item');
-    document.querySelector('.slide').prepend(items[items.length - 1]);
+    let slide = document.querySelector('.slide');
+    slide.prepend(items[items.length - 1]); // Mueve el último elemento al principio
+    slide.style.justifyContent = "flex-start"; // Alinea los elementos al extremo izquierdo
 }
